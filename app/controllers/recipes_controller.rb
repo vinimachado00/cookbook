7c10155c858ae16aa2b37fae761b90d9405a5a99
Vini Santana <viniksantana5@gmail.com>
2019-03-03 05:06:05 -0300

    User register recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,21 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(params_recipe)
+    redirect_to @recipe
+  end
+
+  private
+
+  def params_recipe
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine,
+                                   :difficulty, :cook_time,
+                                   :ingredients, :cook_method)
+  end
 end
