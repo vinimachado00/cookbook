b41d915c881fc35cde769e8d12341ff401db15e3
Vini Santana <viniksantana5@gmail.com>
2019-03-08 22:51:45 -0300

    User removes recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -43,6 +43,12 @@ class RecipesController < ApplicationController
     end
   end
 
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to root_path
+  end
+
   private
 
   def params_recipe
---
+++ spec/features/user_removes_recipe_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'User removes recipe' do
+  scenario 'successfully' do
+    # cria os dados
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    cuisine = Cuisine.create(name: 'Brasileira')
+    another_recipe_type = RecipeType.create(name: 'Prato principal')
+    Recipe.create(title: 'Bolo de cenoura', difficulty: 'Médio',
+                  recipe_type: recipe_type, cuisine: cuisine,
+                  cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                  cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+    
+    Recipe.create(title: 'Feijoada', recipe_type: another_recipe_type,
+                  cuisine: cuisine, difficulty: 'Difícil',
+                  cook_time: 90,
+                  ingredients: 'Feijão e carnes',
+                  cook_method: 'Misture o feijão com as carnes')
+    # ação do usuário
+    visit root_path
+    click_on 'Bolo de cenoura'
+    click_on 'Apagar receita'
+
+    # expectativas
+    expect(page).not_to have_css('h1', text: 'Bolo de cenoura')
+    expect(page).to have_css('h1', text: 'Feijoada')
+  end
+end
