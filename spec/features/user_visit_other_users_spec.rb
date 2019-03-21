983243331043bd36c53ea432473b526e9c85f889
Vini Santana <viniksantana5@gmail.com>
2019-03-21 02:02:12 -0300

    User visit other users - finished

--- spec/features/user_visit_other_users_spec.rb
+++ spec/features/user_visit_other_users_spec.rb
@@ -17,4 +17,35 @@ feature 'User visit other users' do
     expect(page).to have_css('li', text: recipe1.title)
     expect(page).to have_css('li', text: recipe2.title)
   end
+
+  scenario 'and access recipe' do
+    user1 = create(:random_user)
+    user2 = create(:user)
+    recipe1 = create(:recipe, user: user2)
+
+    login_as user1, scope: :user
+    visit user_path(user2)
+    click_on recipe1.title
+
+    expect(current_path).to eq recipe_path(recipe1)
+    expect(page).to have_css('h1', text: recipe1.title)
+    expect(page).to have_css('h3', text: 'Detalhes')
+    expect(page).to have_css('p', text: recipe1.recipe_type.name)
+    expect(page).to have_css('p', text: recipe1.cuisine.name)
+    expect(page).to have_css('p', text: recipe1.difficulty)
+    expect(page).to have_css('p', text: "#{recipe1.cook_time} minutos")
+    expect(page).to have_css('h3', text: 'Ingredientes')
+    expect(page).to have_css('p', text: recipe1.ingredients)
+    expect(page).to have_css('h3', text: 'Como Preparar')
+    expect(page).to have_css('p', text: recipe1.cook_method)    
+  end
+
+  scenario 'and must be logged in' do
+    user = create(:user)
+    recipe = create(:recipe, user: user)
+
+    visit user_path(user)
+
+    expect(current_path).to eq new_user_session_path
+  end
 end
